2237c0c17278ba10e631e4a370489b6ac119ea4f
Don Moses <githinjidon@gmail.com>
2020-03-02 11:08:59 +0300

    add procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn mestagram.wsgi --log-file -
--- pics/models.py
+++ pics/models.py
@@ -9,12 +9,24 @@ class Location(models.Model):
     def __str__(self):
         return self.location
 
+    def save_location(self):
+        self.save()
+
+    def delete_location(self):
+        self.delete()
+
 
 class Category(models.Model):
     category = models.CharField(max_length=10)
 
     def __str__(self):
         return self.category
+
+    def save_category(self):
+        self.save()
+
+    def delete_category(self):
+        self.delete()
 
 
 class Image(models.Model):
