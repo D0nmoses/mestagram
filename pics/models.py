45bfb8b4111e3d0786c2095b92f0b88fa105617d
Don Moses <githinjidon@gmail.com>
2020-03-02 09:18:04 +0300

    add search url to urls.py

--- pics/models.py
+++ pics/models.py
@@ -38,7 +38,7 @@ class Image(models.Model):
 
     @classmethod
     def search_by_category(cls, search_term):
-        images = cls.objects.filter(title__icontains=search_term)
+        images = cls.objects.filter(category__icontains=search_term)
         return images
 
 
