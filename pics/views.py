bdddc70bf39ad1c8daaee6d8a9a5d623a96dd40e
Don Moses <githinjidon@gmail.com>
2020-03-02 09:13:51 +0300

    define search.html and add view for search

--- pics/views.py
+++ pics/views.py
@@ -7,3 +7,16 @@ def index(request):
     images = Image.all_images()
 
     return render(request, 'all-pics/index.html',{"images":images})
+
+def search_results(request):
+
+    if 'image' in request.GET and request.GET["image"]:
+        search_term = request.GET.get("image")
+        searched_images = Image.search_by_category(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'all-pics/search.html',{"message":message,"images": searched_images})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'all-pics/search.html',{"message":message})
