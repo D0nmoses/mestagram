c6edc7fb7703d6a55c3b10b27740381e42bd86c4
Don Moses <githinjidon@gmail.com>
2020-03-02 11:01:27 +0300

    add modal definition for search results

--- pics/urls.py
+++ pics/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 urlpatterns=[
     re_path(r'^$',views.index,name='index'),
     re_path(r'^search/', views.search_results, name='search_results'),
+
 ]
 
 if settings.DEBUG:
